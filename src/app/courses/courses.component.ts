cf172e13801ba58092b51d561f177d3398dfcb61
bugsdeepak <antonyb.deepak@gmail.com>
2025-02-06 15:52:34 -0600

    intial set of files

---
+++ src/app/courses/courses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'courses',
+  template: "<h2>Courses</h2>",
+  imports: [],
+  //templateUrl: './courses.component.html',
+  //styleUrl: './courses.component.css'
+})
+export class CoursesComponent {
+
+}
