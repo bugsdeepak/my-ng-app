82c0a091172b747d61298c7972c7661a757e5dda
bugsdeepak <antonyb.deepak@gmail.com>
2025-02-07 12:11:25 -0600

    component created. Dependency injection and service created.

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CoursesService } from './courses.service';
 
 @Component({
-  selector: 'courses',
-  template: "<h2>Courses</h2>",
-  imports: [],
-  //templateUrl: './courses.component.html',
-  //styleUrl: './courses.component.css'
+  selector: 'courses', // This is simple tag. For div id use #courses for class use .courses
+  //template: `<h2>{{ title }}</h2>`,
+  //template: `  //<h2>{{ title }}</h2>
+  //   //<ul>
+  //  <li *ngFor="let course of courses">
+  //    {{ course }}
+  //  </li>
+  //  </ul>
+  //`,
+  imports: [CommonModule],  
+  templateUrl: './courses.component.html',
+  styleUrl: './courses.component.css'
 })
+
 export class CoursesComponent {
+
+  title = 'List of Courses';
+  courses ;  //= ["Java", "JavaScript", "TypeScript"];
+  
+  //Dependency Injection ==> we are injecting the CoursesService into the constructor of the CoursesComponent class
+  //Register the dependency in the module as a "provider". This is a singleton object.
+  constructor (service: CoursesService) {
+    //let service = new CoursesService();
+    this.courses = service.getCourses();
+  }
 
 }
